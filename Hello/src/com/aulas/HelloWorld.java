dcca3e8f787dfdf2365f4cdf44a8754cab3c070c
Manuel Nunes Pereira <manuel.nupe@gmail.com>
2017-10-05 11:18:02 -0300

    meu terceiro commit

--- Hello/src/com/aulas/HelloWorld.java
+++ Hello/src/com/aulas/HelloWorld.java
@@ -2,6 +2,6 @@ package com.aulas;
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.println("Hello World com Egit");
+	System.out.println("Hello World com Egit!");
 }
 }
